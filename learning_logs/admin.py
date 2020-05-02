ff39954e3f44c798deacea0291438fcaa6a2e9df
yutao <yutao2013@126.com>
2020-05-02 21:14:05 +0800

    模型Entry

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from learning_logs.models import Topic
+from learning_logs.models import Topic,Entry
 # Register your models here.
 # 在这里注册你的模型
 
 admin.site.register(Topic)
+admin.site.register(Entry)
